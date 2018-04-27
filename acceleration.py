b0c611c8054411ab9d71b42f21feeb4c14bcb845
PKL888 <pelegkl888@gmail.com>
2018-04-27 17:24:42 +1000

    Working on program

--- acceleration.py
+++ acceleration.py
@@ -1,37 +1,40 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
- 
+
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 100), ylim=(0, 100))
 line, = ax.plot([], [], lw=2)
- 
-# initialization function: plot the background of each frame
+
+
+# Initialization function: plot the background of each frame
 def init():
     line.set_data([], [])
     return line,
- 
-# animation function.  This is called sequentially
+
+
+# Animation function. This is called sequentially
 def animate(i):
 
-	x = np.linspace(0, 100, 101)
-	y = np.linspace(0, 100, 101)
+    x = np.linspace(0, 100, 101)
+    y = np.linspace(0, 100, 101)
 
-	g = 9.8
+    g = 9.8
 
-    	for i in np.linspace(0, 98, 99):
-	    r = int(i)			# n
-	    s = int(r + 1)		# n + 1
-	    t = int(r + 2)		# n + 2
+    for i in np.linspace(0, 98, 99):
+        r = int(i)			# n
+        s = int(r + 1)		# n + 1
+        t = int(r + 2)		# n + 2
 
-	    y[t] = (2 * y[s] - y[r] - g * ((0.1) ** 2))
-    	line.set_data(x, y[t])
+        y[t] = (2 * y[s] - y[r] - g * ((0.1) ** 2))
+        line.set_data(x, y[t])
 
-    	return line,
- 
-# call the animator.  blit=True means only re-draw the parts that have changed.
+        return line,
+
+
+# Call the animator. blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=200, interval=20, blit=True)
- 
+
 plt.show()
