efb97412a9414defa419c38cd7fc3cd90cca24dc
PKL888 <pelegkl888@gmail.com>
2018-04-29 15:42:57 +1000

    Added explanation of the calculation

--- acceleration.py
+++ acceleration.py
@@ -15,6 +15,7 @@ for i in np.linspace(0, 98, 99):
     s = int(i + 1)
     t = int(i + 2)
 
+    x[t] = (2 * x[s] - x[r] + 0)
     y[t] = (2 * y[s] - y[r] - g * ((0.1) ** 2))
 
     if y[t] < 0:
@@ -29,3 +30,40 @@ for i in np.linspace(0, 98, 99):
     ax.scatter(x[r], y[t])
 
 plt.show()
+
+# Velocity = change in location / change in time
+
+# m --> mass
+# g --> gravity (the force of)
+# f = m * a
+# f = m * g
+#            ==> mg = ma
+#                ==> a = g = 9.81 (m / s ** 2)
+
+# a = ((y(n+2) - y(n+1)) / delta of 't' - (y(n+1) - y(n)) /,
+# delta of 't') / delta of 't' ** 2 = g
+
+# a =   y(n+2) - y(n+1)   y(n+1) - y(n)     = g
+#       --------------- - -------------
+#              t                t
+#     -----------------------------------
+#                     t ** 2
+
+# a =   y(n+2) - 2y(n+1) + y(n)     = g
+#       -----------------------
+#               t ** 2
+
+# y(n+2) - 2y(n+1) + y(n) = g * (t ** 2)
+
+# y(n+2) = 2y(n+1) - y(n) - g * (t ** 2)
+# x(n+2) = 2x(n+1) - x(n)
+
+"""
+        |          x    x
+        |      x           x
+    y(1)|____.                x
+    y(0)|__.'|                   x
+      __|__|_|_____                x
+        |x(0) x(1)
+
+"""
